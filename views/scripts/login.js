9d1f2f5a7352dd599fabe245d197fe9cb4714b74
Felix-Jacob <jacobfelix51@gmail.com>
2022-06-14 12:58:42 +0200

    test

--- views/scripts/login.js
+++ views/scripts/login.js
@@ -1,1 +1,17 @@
-console.log('hello');
+
+window.onload = function() {
+    buttonLogin = document.getElementById('buttonLogin');
+    buttonRegister = document.getElementById('buttonRegister');
+    
+    buttonLogin.addEventListener('click', login);
+};
+
+async function login() {
+    console.log('login pressed');
+    let enteredUsername = document.getElementById('inputUsername').value;
+    let enteredPassword = document.getElementById('inputPassword').value;
+}
+
+async function register() {
+
+}
