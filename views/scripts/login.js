c31df1bd470abbc7dd2565d7407dca09c1f3464b
Felix-Jacob <jacobfelix51@gmail.com>
2022-06-14 13:15:16 +0200

    Added DB login req

--- views/scripts/login.js
+++ views/scripts/login.js
@@ -10,6 +10,20 @@ async function login() {
     console.log('login pressed');
     let enteredUsername = document.getElementById('inputUsername').value;
     let enteredPassword = document.getElementById('inputPassword').value;
+
+    let response = await fetch('16996205-7370-4149-bed2-ae69b3073539.ul.bw-cloud-instance.org:4000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: enteredUsername,
+            password: enteredPassword
+        })
+    });
+
+    let responseJSON = await response.json();
+    console.log(responseJSON);
 }
 
 async function register() {
