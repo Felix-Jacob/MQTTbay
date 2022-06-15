d42782039c6443777e21646eca555e3c713bf767
Felix-Jacob <jacobfelix51@gmail.com>
2022-06-15 14:22:35 +0200

    fs

--- views/scripts/login.js
+++ views/scripts/login.js
@@ -19,7 +19,7 @@ async function login() {
 
     let responseText = await response.text();
 
-    console.log(responseText);
+    console.log(`fetch response: ${responseText}`);
 }
 
 async function register() {
