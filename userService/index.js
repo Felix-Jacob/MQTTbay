85f422e82b12a4270536e351e615112e6e87ea59
Felix <jacobfelix51@gmail.com>
2022-06-16 13:54:51 +0200

    dfs

--- userService/index.js
+++ userService/index.js
@@ -54,6 +54,7 @@ app.post('/login', cors(corsOptions), (req, res) => {
 
 	else {
 		let accessToken = jwt.sign(enteredUsername, rand);
+		return res.status(200).json({'token': accessToken});
 	}
 });
 
