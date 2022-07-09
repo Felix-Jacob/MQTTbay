59b82497651ee28d19e2d540fc4a228a9f9d3612
Felix <jacobfelix51@gmail.com>
2022-07-09 17:01:33 +0200

    refactoring

--- mqtt_moderator/index.js
+++ mqtt_moderator/index.js
@@ -1,27 +1,46 @@
 const mqtt = require('mqtt');
 const client = mqtt.connect();
 const bidTopic = 'bidReq/+/+';
+const registerTopic = 'register';
+var registeredUsers = [];
 
 client.on('connect', function () {
   client.subscribe(bidTopic, {qos:2}, (err, granted) => {
     console.log('successfully subscribed to: ', bidTopic);
   });
+  client.subscribe(registerTopic, {qos:2}, (err, granted) => {
+    console.log('successfully subscribed to: ', registerTopic);
+  });
 });
 
 client.on('message', function (topic, message) {
+  let rootTopic = topic.split('/')[0];
   let articleId = topic.split('/')[1];
   let userId = topic.split('/')[2];
-  let bid = message.toString();
-  
-  if(checkBid(bid)) {
-    client.publish(`bidRes/${articleId}/${userId}`, {qos:2}, (err) => {
-      if(err) 
-        console.log(err);
-    });
-  } else {
-  }
+
+  if (rootTopic == 'register')
+    processRegistrationMessage(message);
+  else if(rootTopic == 'bidReq')
+    processBidMessage(message);
 });
 
 function checkBid(bid) {
   return true;
+}
+
+function processBidMessage(message) {
+  let bid = message.toString();
+  if(checkBid(bid)) {
+    client.publish(`bidRes/${articleId}/${userId}`, 'success',  {qos:2}, (err) => {
+      if(err) 
+        console.log(err);
+    });
+  } else {
+    client.publish(`bidRes/${articleId}/${userId}`, 'error',  {qos:2}, (err) => {
+    });
+  }
+}
+
+function processRegistrationMessage(message) {
+
 }
