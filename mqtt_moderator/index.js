ba588f08ffbeb89ec994324abfcfd73e42166668
Felix <jacobfelix51@gmail.com>
2022-07-10 13:34:09 +0200

    refactoring

--- mqtt_moderator/index.js
+++ mqtt_moderator/index.js
@@ -19,8 +19,6 @@ client.on('connect', function () {
 
 client.on('message', function (topic, message) {
   let rootTopic = topic.split('/')[0];
-  let articleId = topic.split('/')[1]; // TODO: move this to place its needed
-  let userId = topic.split('/')[2];
 
   if (rootTopic == 'register') {
     processRegistrationMessage(message);
@@ -31,7 +29,9 @@ client.on('message', function (topic, message) {
     }
     else {
       if(rootTopic == 'bidReq') {
-        processBidMessage(message);
+        let articleId = topic.split('/')[1]; 
+        let userId = topic.split('/')[2];
+        processBidMessage(message, articleId, userId);
       }
     }
   }
@@ -41,7 +41,7 @@ function checkBid(bid) {
   return true;
 }
 
-function processBidMessage(message) {
+function processBidMessage(message, articleId, userId) {
   let bid = message.toString();
   if(checkBid(bid)) {
     client.publish(`bidRes/${articleId}/${userId}`, 'success',  {qos:2}, (err) => {
@@ -63,7 +63,7 @@ function processUnregistrationMessage(message) {
   registeredUsers.find((userId, index) => {
     if(userId == message) {
       registeredUsers.splice(index, 1);
-      return true; // eigentlich unnoetig, da userId einzigartig
+      return true; 
     }
   });
   console.log(registeredUsers);
