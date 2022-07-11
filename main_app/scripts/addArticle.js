fc5d24ac58b938e5f16a58aedd210e3499bee38d
Felix <jacobfelix51@gmail.com>
2022-07-11 16:19:44 +0200

    article fronten

--- main_app/scripts/addArticle.js
+++ main_app/scripts/addArticle.js
@@ -2,12 +2,44 @@ window.onload = async function() {
 
     if(checkAccessToken()) {
         setupMQTTConnection();
-        buttonAddArticle = document.getElementById('buttonBack');
+        buttonAddArticle = document.getElementById('buttonAdd');
         buttonAddArticle.addEventListener('click', () => {
-            document.location.href = origin + "/main";
-        });        
+          addArticle();
+        });       
+
+        buttonBack = document.getElementById('buttonBack');
+        buttonBack.addEventListener('click', () => {
+          document.location.href = origin + "/main";
+        });       
 }
     else {
         document.location.href = origin + "/login";
     }
 };
+
+function getArticleDetails() {
+    let generatedArticleId = uuidv4();
+    let enteredArticleName = document.getElementById('inputArticleName').value;
+    let enteredArticlePrice = document.getElementById('inputArticlePrice').value;
+    let userName = 'dummy';
+
+    return {articleId: generatedArticleId, articleName: enteredArticleName, articlePrice: enteredArticlePrice, userName: userName};
+}
+
+async function addArticle() {
+  enteredArticleDetails = getArticleDetails();
+
+  let response = await fetch('http://16996205-7370-4149-bed2-ae69b3073539.ul.bw-cloud-instance.org:4000/addArticle', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(enteredArticleDetails)
+  });
+
+  let responseOk = await response.ok;
+
+  if(responseOk) {}
+
+  console.log(`fetch response: ${response.status}`);
+}
